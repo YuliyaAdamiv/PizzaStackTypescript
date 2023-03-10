3e10f2e840aedf4dc830238df7fd65a806f58ab6
Yuliya <yulua03071992@gmail.com>
2023-03-10 14:17:51 +0200

    fix warning to menu item

--- src/common/componets/menu/menu-item/MenuItem.tsx
+++ src/common/componets/menu/menu-item/MenuItem.tsx
@@ -10,7 +10,7 @@ export const MenuItem:FC<MenuItemProps>=({imagePath, weight, title, ingredients,
     return <div className="w-96 shadow-xl rounded-2xl bg-white">
         <div className="relative">
             <img className="object-cover  object-center w-full h-[15rem] rounded-t-2xl" 
-            src={imagePath}/>
+            src={imagePath} alt={title}/>
         <span className="absolute bottom-1.5 right-3 bg-gray-900/50 text-white text-sm px-2 rounded-[2rem]">{weight} г</span>
         </div>
         <div className="p-8">
